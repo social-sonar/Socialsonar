0418df1d5034b747c454093d192666e5b78a6392
Christian Herrera <christian.herrera@azumo.co>
2024-04-09 17:15:04 -0600

    refactor: independent OAuth2 flow
    Add the OAuth2Client as dependencies. This
    avoids issues when there are multiple users

--- next/src/lib/integrations/google.ts
+++ next/src/lib/integrations/google.ts
@@ -1,31 +1,38 @@
-import { google, people_v1 } from 'googleapis'
+import { google, Auth } from 'googleapis';
+import { GoogleResponse } from '../definitions';
 
+// Define scopes and redirect URL
 const SCOPES = ['https://www.googleapis.com/auth/contacts.readonly'];
 
-
-const oauth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URL,
-);
-
-export function authUrl(): string {
-    const url = oauth2Client.generateAuthUrl({
-        scope: SCOPES,
-        redirect_uri: process.env.REDIRECT_URL,
-        prompt: 'consent'
-    });
-    return url
+export function createOAuth2Client(tokens?: Auth.Credentials): Auth.OAuth2Client {
+    const oauth2Client = new google.auth.OAuth2(
+        process.env.CLIENT_ID,
+        process.env.CLIENT_SECRET,
+        process.env.REDIRECT_URL,
+    );
+    
+    if (tokens) {
+        oauth2Client.setCredentials(tokens);
+    }
+    
+    return oauth2Client;
 }
 
-export async function sync(code: string): Promise<people_v1.Schema$Person[]> {
-    const { tokens } = await oauth2Client.getToken(code)
+export function generateAuthUrl(oauth2Client: Auth.OAuth2Client): string {
+    return oauth2Client.generateAuthUrl({
+        scope: SCOPES,
+        prompt: 'consent'
+    });
+}
+
+export async function syncContacts(oauth2Client: Auth.OAuth2Client, code: string): Promise<GoogleResponse[]> {
+    const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
-    const people = google.people({ version: 'v1', auth: oauth2Client })
+    const people = google.people({ version: 'v1', auth: oauth2Client });
     const res = await people.people.connections.list({
         resourceName: 'people/me',
-        sortOrder: 'LAST_MODIFIED_DESCENDING', 
-        pageSize: 10,
+        sortOrder: 'LAST_MODIFIED_DESCENDING',
+        pageSize: 1000,
         personFields: 'names,emailAddresses,addresses,phoneNumbers,photos,organizations,occupations',
     });
     return res.data.connections || [];
