59139fe11339d0a5d4291ac688b9617033357cec
Christian Herrera <christian.herrera@azumo.co>
2024-04-09 17:15:04 -0600

    refactor: independent OAuth2 flow
    Add the OAuth2Client as dependencies. This
    avoids issues when there are multiple users

--- next/src/app/sync/page.tsx
+++ next/src/app/sync/page.tsx
@@ -1,10 +1,11 @@
 import SyncButton from "@/components/Sync";
-import { authUrl } from "@/lib/integrations/google";
+import { createOAuth2Client, generateAuthUrl } from "@/lib/integrations/google";
 import logoGoogleContacts from '@/images/logos/google-contacts.svg'
 
 
 export default async function Page() {
-    const url = authUrl()
+    const oauth = createOAuth2Client()
+    const url = generateAuthUrl(oauth)
     return <div>
         <SyncButton authUrl={url} logo={logoGoogleContacts} title="Google contacts"/>
     </div>
