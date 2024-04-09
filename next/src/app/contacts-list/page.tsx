5bcce3f42a3c791fca969e30310d2c2adfd5dab3
Christian Herrera <christian.herrera@azumo.co>
2024-04-09 17:17:29 -0600

    refactor: retrieve contact data from the DB

--- next/src/app/contacts-list/page.tsx
+++ next/src/app/contacts-list/page.tsx
@@ -1,61 +1,9 @@
-import { sync } from "@/lib/integrations/google"
-import { people_v1 } from 'googleapis'
 
+import { findContacts, syncGoogleContacts } from "@/lib/data"
+import { GoogleResponse } from "@/lib/definitions"
+import { syncContacts, createOAuth2Client } from "@/lib/integrations/google"
+import Link from "next/link"
 
-const people = [
-  {
-    name: 'Leslie Alexander',
-    email: 'leslie.alexander@example.com',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    lastSeen: '3h ago',
-    lastSeenDateTime: '2023-01-23T13:23Z',
-  },
-  {
-    name: 'Michael Foster',
-    email: 'michael.foster@example.com',
-    role: 'Co-Founder / CTO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    lastSeen: '3h ago',
-    lastSeenDateTime: '2023-01-23T13:23Z',
-  },
-  {
-    name: 'Dries Vincent',
-    email: 'dries.vincent@example.com',
-    role: 'Business Relations',
-    imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    lastSeen: null,
-  },
-  {
-    name: 'Lindsay Walton',
-    email: 'lindsay.walton@example.com',
-    role: 'Front-end Developer',
-    imageUrl:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    lastSeen: '3h ago',
-    lastSeenDateTime: '2023-01-23T13:23Z',
-  },
-  {
-    name: 'Courtney Henry',
-    email: 'courtney.henry@example.com',
-    role: 'Designer',
-    imageUrl:
-      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    lastSeen: '3h ago',
-    lastSeenDateTime: '2023-01-23T13:23Z',
-  },
-  {
-    name: 'Tom Cook',
-    email: 'tom.cook@example.com',
-    role: 'Director of Product',
-    imageUrl:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    lastSeen: null,
-  },
-]
 
 export default async function Example(
   {
@@ -66,18 +14,23 @@ export default async function Example(
     }
   }
 ) {
-  let results: people_v1.Schema$Person[] = []
   if (searchParams?.code) {
-    results = await sync(searchParams.code)
+    const oauth = createOAuth2Client()
+    const results: GoogleResponse[] = await syncContacts(oauth, searchParams.code)
+    await syncGoogleContacts(results)
+  }
+  const contacts = await findContacts(1n) // dummy search
+  if (contacts.length == 0) {
+    return <p>No contacts yet. Sync your contacts <Link href={'/sync'} className="text-teal-500">here</Link></p>
   }
   return (
     <ul role="list" className="divide-y divide-gray-800 max-w-7xl w-full px-24" >
-      {results.map((person) => (
-        <li key={person.phoneNumbers![0].canonicalForm} className="flex justify-between gap-x-6 py-5">
+      {contacts.map((contact) => (
+        <li key={contact.phoneNumber} className="flex justify-between gap-x-6 py-5">
           <div className="flex min-w-0 gap-x-4">
             <img
               className="h-12 w-12 flex-none rounded-full bg-gray-800"
-              src={person.photos && person.photos[0].url ? person.photos[0].url : '/default.png'}
+              src={contact.photoUrl || '/default.png'}
               alt=""
               width={25} height={25}
             />
@@ -85,19 +38,19 @@ export default async function Example(
               <div className="flex">
 
                 <p className="text-sm font-semibold leading-6 text-white">
-                  {person.names && person.names[0].displayName}
+                  {contact.name}
                 </p>
                 <p className="mt-1 truncate text-sm leading-5 text-gray-400 ml-2">
-                  {person.phoneNumbers![0].canonicalForm}
+                  {contact.phoneNumber}
                 </p>
               </div>
               <p className="mt-1 truncate text-xs leading-5 text-gray-400">
-                {person.emailAddresses && person.emailAddresses[0].value ? person.emailAddresses[0].value : "No email address"}
+                {contact.email || "No email address"}
               </p>
             </div>
           </div>
           <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <p className="text-sm leading-6 text-white">{person.occupations && person.occupations[0].value ? person.occupations[0].value : "Occupation not found"}</p>
+            <p className="text-sm leading-6 text-white">{contact.occupation || "Occupation not found"}</p>
             {'3h ago' ? (
               <p className="mt-1 text-xs leading-5 text-gray-400">
                 Last seen{' '}
