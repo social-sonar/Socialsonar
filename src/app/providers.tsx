f36c92a68426c435dbe79a24ab9d12240190d644
Christian Herrera <christian.herrera@azumo.co>
2024-04-16 16:05:08 -0600

    feat: google auth support

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { createContext, useEffect, useRef } from 'react'
 import { usePathname } from 'next/navigation'
 import { ThemeProvider, useTheme } from 'next-themes'
+import { SessionProvider } from 'next-auth/react'
 
 function usePrevious<T>(value: T) {
   let ref = useRef<T>()
@@ -45,11 +46,13 @@ export function Providers({ children }: { children: React.ReactNode }) {
   let previousPathname = usePrevious(pathname)
 
   return (
-    <AppContext.Provider value={{ previousPathname }}>
-      <ThemeProvider attribute="class" disableTransitionOnChange>
-        <ThemeWatcher />
-        {children}
-      </ThemeProvider>
-    </AppContext.Provider>
+    <SessionProvider>
+      <AppContext.Provider value={{ previousPathname }}>
+        <ThemeProvider attribute="class" disableTransitionOnChange>
+          <ThemeWatcher />
+          {children}
+        </ThemeProvider>
+      </AppContext.Provider>
+    </SessionProvider>
   )
 }
