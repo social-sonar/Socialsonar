95989acea3a5c99673235419dbd3fc718ac6013a
Christian Herrera <christian.herrera@azumo.co>
2024-04-18 15:30:40 -0600

    feat: add middleware for route protection

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server"
+
+import { auth } from "@/auth"
+
+
+export default async function (request: any) {
+    // routes protection works as expected when the user is unauthenticated. However, if the user logs in this specific statement `const session = await auth(request)` throws the following error:
+    // **PrismaClient is not configured to run in Vercel Edge Functions or Edge Middleware**
+
+    // this workaround fixes the above as 'authjs.session-token' only exists when the user is authenticated
+    // Note that as far as I know, nextjs handles cookies as HTTP-only
+    const cookieData: Map<string, any> = request.cookies._parsed
+    if (!cookieData.get('authjs.session-token')) {
+        const session = await auth(request)
+        if (!session?.user) {
+            return NextResponse.redirect(new URL('/', request.url))
+        }
+    }
+}
+
+// protect all routes except the ones that match this pattern
+export const config = { matcher: ['/((?!$|api|_next/static|_next/image|favicon.ico).*)'] }
