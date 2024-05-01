38b46a1bcd3a4c7dec6f00b91ca3a2c9b1272028
Christian Herrera <christian.herrera@azumo.co>
2024-05-01 12:23:36 -0600

    fix: add size to LoadingSpinner using the style attr

--- src/components/common/spinner.tsx
+++ src/components/common/spinner.tsx
@@ -1,6 +1,9 @@
 export default function LoadingSpinner({ size }: { size: number }) {
-    const dimmensions = `h-[${size}px] w-[${size}px]`
+    const spinnerDimmensions = {
+        width: `${size}px`,
+        height: `${size}px`,
+    }
     return (
-        <div className={`border-gray-300 rounded-full border-2 animate-spin border-t-blue-600 ${dimmensions}`} />
+        <div className="animate-spin rounded-full border-2 border-gray-300 border-t-blue-600" style={spinnerDimmensions} />
     )
 }
