e4adeb33762f4060b8e83f8f9da1bfe8e845c6e0
Christian Herrera <christian.herrera@azumo.co>
2024-05-03 09:51:20 -0600

    fix: resolve build errors caused by incorrect typing

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,4 +2,4 @@ import nextauth from "@/auth"
 export const GET = nextauth.handlers.GET
 export const POST = nextauth.handlers.POST
 
-export const runtime: string = process.env.NODE_ENV === 'production' ? 'edge' : 'nodejs';
+export const runtime: "edge" | "nodejs" | "experimental-edge" | undefined = process.env.NODE_ENV === 'production' ? 'edge' : 'nodejs';
