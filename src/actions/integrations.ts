d090c88e3beb9948b14bc7cd0b32af36e19ac43d
Christian Herrera <christian.herrera@azumo.co>
2024-05-09 16:04:12 -0600

    fix: handle invalid credentials error
    Add strategy to refresh the access token related
    to google contacts

--- src/actions/integrations.ts
+++ src/actions/integrations.ts
@@ -1,20 +1,32 @@
 'use server'
 
-import { google } from 'googleapis';
+import nextauth from '@/auth';
 import prisma from '@/db';
-import { redirect } from 'next/navigation';
 import { syncGoogleContacts } from '@/lib/data/common';
-import nextauth from '@/auth'
-import { CustomSession } from '@/lib/definitions';
+import { CustomSession, GoogleContactMainResponse } from '@/lib/definitions';
+import { GaxiosResponse } from 'gaxios';
+import { OAuth2Client } from 'google-auth-library';
+import { google } from 'googleapis';
+import { redirect } from 'next/navigation';
+
+async function refreshToken(session: CustomSession, oauth2Client: OAuth2Client) {
+    oauth2Client.setCredentials({ refresh_token: session.refreshToken });
+    const { credentials } = await oauth2Client.refreshAccessToken();
+    session.accessToken = credentials.access_token!;
+    session.expiresAt = Date.now() + credentials.expiry_date!;
+    oauth2Client.setCredentials({ access_token: session.accessToken });
+}
 
 export async function fetchGoogleContacts(userId: string) {
-    // Create a new OAuth2 client with your app's credentials and set the access token
+    const session = await nextauth.auth() as CustomSession
+
+
     const oauth2Client = new google.auth.OAuth2(
         process.env.GOOGLE_CLIENT_ID,
         process.env.GOOGLE_CLIENT_SECRET,
         process.env.REDIRECT_URL,
     );
-    const session = await nextauth.auth() as CustomSession
+
 
     oauth2Client.setCredentials({ access_token: session.accessToken });
 
@@ -32,16 +44,33 @@ export async function fetchGoogleContacts(userId: string) {
             token: true
         }
     })
-
     // Fetch the user's Google Contacts
-    const res = await people.people.connections.list({
-        resourceName: 'people/me',
-        sortOrder: 'LAST_MODIFIED_DESCENDING',
-        pageSize: 1000,
-        personFields: 'names,emailAddresses,addresses,phoneNumbers,photos,organizations,occupations,birthdays',
-        requestSyncToken: true,
-        syncToken: syncToken?.token,
-    });
+    let res: GaxiosResponse<GoogleContactMainResponse>
+    try {
+        res = await people.people.connections.list({
+            resourceName: 'people/me',
+            sortOrder: 'LAST_MODIFIED_DESCENDING',
+            pageSize: 1000,
+            personFields: 'names,emailAddresses,addresses,phoneNumbers,photos,organizations,occupations,birthdays',
+            requestSyncToken: true,
+            syncToken: syncToken?.token,
+        });
+    } catch (error: unknown) {
+        try {
+            await refreshToken(session, oauth2Client);
+            res = await people.people.connections.list({
+                resourceName: 'people/me',
+                sortOrder: 'LAST_MODIFIED_DESCENDING',
+                pageSize: 1000,
+                personFields: 'names,emailAddresses,addresses,phoneNumbers,photos,organizations,occupations,birthdays',
+                requestSyncToken: true,
+                syncToken: syncToken?.token,
+            });
+        } catch (error: unknown) {
+            throw error
+        }
+    }
+
 
     if (syncToken?.token) {
         await prisma.googleSyncToken.update({
@@ -62,9 +91,7 @@ export async function fetchGoogleContacts(userId: string) {
         })
     }
 
-    // Handle the response as needed
     if (res.data.connections)
-        // TODO: add logic to handle duplicates
         await syncGoogleContacts(res.data.connections, userId)
     redirect('/contacts-list')
 }
