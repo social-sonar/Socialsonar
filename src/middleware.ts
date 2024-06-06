805ee0ebd16206d6dceaf22be8d82822977333fc
Christian Herrera <christian.herrera@azumo.co>
2024-06-06 16:21:46 -0600

    refactor: allow access to children routes  of '/u/'

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,5 +16,5 @@ export default async function middleware(request : NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!$|api|_next/static|_next/image|favicon.ico).*)']
+    matcher: ['/((?!$|api|_next/static|_next/image|favicon.ico|u).*)']
 };
