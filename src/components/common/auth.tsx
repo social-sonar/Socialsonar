c9f6899d4517614eaef5aa82776192a414b156de
Christian Herrera <christian.herrera@azumo.co>
2024-06-27 09:59:07 -0600

    fix: build errors

--- src/components/common/auth.tsx
+++ src/components/common/auth.tsx
@@ -3,10 +3,10 @@
 import { redirect } from "next/navigation";
 import { useSession } from "next-auth/react";
 
-type AuthenticatedComponent = (props: Record<string, any>) => React.ReactElement | null
+type AuthenticatedComponent = (props: any) => React.ReactElement | null
 
-export const protectPage = (WrappedComponent: React.ComponentType<Record<string, any>>): AuthenticatedComponent => {
-  return function WithAuth(props: Record<string, any>) {
+export const protectPage = (WrappedComponent: React.ComponentType<any>): AuthenticatedComponent => {
+  return function WithAuth(props: any) {
     const session = useSession()
     if (!session.data?.user) {
       redirect("/");
