7fdccc58a5da14230ae0bf181106c95d98e9a13e
Christian Herrera <christian.herrera@azumo.co>
2024-07-16 17:35:38 -0600

    feat: add next action for registering travels

---
+++ src/actions/google/events/travels.ts
@@ -0,0 +1,56 @@
+'use server'
+
+import prisma from '@/db'
+import { sendEventNotification } from './scheduler'
+import tzlookup from '@photostructure/tz-lookup'
+
+type TravelData = {
+  userId: string
+  location: string
+  coords: string // expected to be latitude,longitude
+  startDate: string // expected to have the yyyy-mm-dd format
+  endDate: string // expected to have the yyyy-mm-dd format
+}
+
+export const registerTravel = async ({
+  userId,
+  location,
+  coords,
+  startDate,
+  endDate,
+}: TravelData) => {
+  const [lat, lon] = coords.split(',').map(Number)
+  const googleEventId = await sendEventNotification(userId, (data) => ({
+    summary: `Travel to ${location}`,
+    location,
+    description: `All day event`,
+    organizer: { email: data.email },
+    start: {
+      date: data.startDate,
+    },
+    end: {
+      date: data.endDate,
+    },
+    reminders: {
+      useDefault: false,
+      overrides: [{ method: 'email', minutes: 24 * 60 * 7 }],
+    },
+  }))
+  await prisma.event.create({
+    data: {
+      userId,
+      googleEventId,
+      start: `${startDate}T00:00:00.000Z`,
+      end: `${endDate}T00:00:00.000Z`,
+      timezone: 'not applicable', // events with no datetime have no timezone
+      recurrence: `RRULE:FREQ=DAILY;UNTIL=${endDate.replaceAll('-', '')}T000000Z`,
+      travel: {
+        create: {
+          userId,
+          location,
+          timezone: tzlookup(lat, lon),
+        },
+      },
+    },
+  })
+}
